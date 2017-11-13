7e2e0f72549d217d86e3834e883a5ef87e103286
Jacob Brunsting <jacob.brunsting@gmail.com>
2017-11-12 22:23:17 -0800

    Paginate response

--- main.go
+++ main.go
@@ -9,12 +9,15 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/iced-mocha/shared/models"
+	"github.com/patrickmn/go-cache"
 )
 
 const (
 	defaultPostCount = 20
+	port             = ":4000"
 	feedURL          = "https://hacker-news.firebaseio.com/v0/beststories.json"
 	postURL          = "https://hacker-news.firebaseio.com/v0/item/"
+	baseURL          = "http://localhost" + port
 )
 
 type Response struct {
@@ -34,21 +37,32 @@ type Post struct {
 	URL         string
 }
 
-func GetPosts(w http.ResponseWriter, r *http.Request) {
+func subtract(dataset []int, toRemove []int) []int {
+	inToRemove := make(map[int]bool)
+	for _, id := range toRemove {
+		inToRemove[id] = true
+	}
+
+	toReturn := make([]int, 0)
+	for _, id := range dataset {
+		if !inToRemove[id] {
+			toReturn = append(toReturn, id)
+		}
+	}
+
+	return toReturn
+}
+
+func getNextIDs(allIDs, viewedIDs []int, count int) []int {
+	unviewedIDs := subtract(allIDs, viewedIDs)
+	if len(unviewedIDs) >= count {
+		return unviewedIDs[:count]
+	}
+	return unviewedIDs
+}
+
+func GetPosts(w http.ResponseWriter, r *http.Request, c *cache.Cache, id func() string) {
 	var err error
-
-	resp, err := http.Get(feedURL)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusNotFound)
-		return
-	}
-
-	var postIds []int
-	err = json.NewDecoder(resp.Body).Decode(&postIds)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusNotFound)
-		return
-	}
 
 	queryParams := r.URL.Query()
 	postCountToReturn := defaultPostCount
@@ -59,30 +73,65 @@ func GetPosts(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "'count' could not be converted to an integer", http.StatusNotFound)
 		return
 	}
-	if postCountToReturn > len(postIds) {
-		postCountToReturn = len(postIds)
+
+	var pagingToken string
+	if arr, ok := queryParams["continue"]; ok && len(arr) > 0 {
+		pagingToken = arr[0]
 	}
 
-	respChan := make(chan Response, postCountToReturn)
-	for i := 0; i < postCountToReturn; i++ {
-		id := postIds[i]
+	resp, err := http.Get(feedURL)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	var allPostIDs []int
+	err = json.NewDecoder(resp.Body).Decode(&allPostIDs)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	viewedIDs := make([]int, 0)
+	if v, ok := c.Get(pagingToken); ok {
+		viewedIDs = v.([]int)
+	}
+
+	nextIDs := getNextIDs(allPostIDs, viewedIDs, postCountToReturn)
+
+	var nextURL string
+	viewedIDs = append(viewedIDs, nextIDs...)
+	if len(subtract(allPostIDs, viewedIDs)) != 0 {
+		nextToken := id()
+		nextURL = baseURL + "/v1/posts?continue=" + nextToken + "&count=" + strconv.Itoa(postCountToReturn)
+		c.Set(nextToken, viewedIDs, cache.DefaultExpiration)
+	}
+
+	respChans := make([]chan Response, 0)
+	for _, id := range nextIDs {
+		cID := id
+		respChan := make(chan Response)
+		respChans = append(respChans, respChan)
 		go func() {
-			resp, err := http.Get(postURL + strconv.Itoa(id) + ".json")
+			resp, err := http.Get(postURL + strconv.Itoa(cID) + ".json")
 			respChan <- Response{resp, err}
 		}()
 	}
 
 	var postsToReturn []models.Post
-	for i := 0; i < postCountToReturn; i++ {
+	for _, respChan := range respChans {
 		response := <-respChan
 		err := response.err
 		resp := response.resp
+		defer resp.Body.Close()
 		if err != nil {
+			log.Printf("error getting post: %v", err)
 			continue
 		}
 		var post Post
 		err = json.NewDecoder(resp.Body).Decode(&post)
 		if err != nil {
+			log.Printf("error decoding response body %v: %v", resp.Body, err)
 			continue
 		}
 		postToReturn := models.Post{
@@ -95,7 +144,8 @@ func GetPosts(w http.ResponseWriter, r *http.Request) {
 		postsToReturn = append(postsToReturn, postToReturn)
 	}
 
-	res, err := json.Marshal(postsToReturn)
+	cRes := models.ClientResp{postsToReturn, nextURL}
+	res, err := json.Marshal(cRes)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -106,7 +156,21 @@ func GetPosts(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	var idCounter int32
+	id := func() string {
+		idCounter++
+		return strconv.FormatInt(int64(idCounter), 32)
+	}
+
+	var c *cache.Cache
+	c = cache.New(30*time.Minute, 45*time.Minute)
+
+	f := func(w http.ResponseWriter, r *http.Request) {
+		GetPosts(w, r, c, id)
+	}
+
 	r := mux.NewRouter()
-	r.HandleFunc("/v1/posts", GetPosts).Methods(http.MethodGet)
-	log.Fatal(http.ListenAndServe(":4000", r))
+	r.HandleFunc("/v1/posts", f).Methods(http.MethodGet)
+	log.Printf("starting server on port " + port)
+	log.Fatal(http.ListenAndServe(port, r))
 }
